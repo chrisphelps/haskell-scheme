1834d50e1953d36c3f9af7b291f653c17d74cb0e
Chris Phelps <cphelps@tendrilinc.com>
2015-02-15 21:06:53 -0700

    Read the line in and spit it out

--- hello.hs
+++ hello.hs
@@ -2,8 +2,8 @@ module Main where
 import System.Environment
  
 main :: IO ()
-main = total getArgs
-
+--main = total getArgs
+main = promptForName getArgs
     
 total :: IO [String] -> IO ()
 total cmdline = do
@@ -11,3 +11,9 @@ total cmdline = do
     let nums = fmap read args :: [Int] -- hint that we want [Int]
         total = foldl (+) 0 nums
     putStrLn ("Total = " ++ show total)
+
+promptForName :: IO [String] -> IO ()
+promptForName cmdline = do
+    putStrLn "Enter a name"
+    name <- getLine
+    putStrLn ("Hello, " ++ name)

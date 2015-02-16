d67298a92a62718300b121a0310f772ba4a0cee4
Chris Phelps <cphelps@tendrilinc.com>
2015-02-15 18:45:47 -0700

    Move arg totaling to separate function

--- hello.hs
+++ hello.hs
@@ -2,10 +2,12 @@ module Main where
 import System.Environment
  
 main :: IO ()
-main = do
-    args <- getArgs
-    let nums = fmap read args :: [Int] -- hint that we want [Int]
-    	total = foldl (+) 0 nums
-    putStrLn ("Total = " ++ show total)
+main = total getArgs
 
     
+total :: IO [String] -> IO ()
+total cmdline = do
+    args <- cmdline
+    let nums = fmap read args :: [Int] -- hint that we want [Int]
+        total = foldl (+) 0 nums
+    putStrLn ("Total = " ++ show total)
